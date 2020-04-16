93e817a3d9b6b12d49affb180159eff9f99426a1
Veit Lehmann <veit.lehmann@aida.de>
2020-04-16 10:17:41 +0200

    improve passing options

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const mandelbrot = require('@frctl/mandelbrot')
 
 module.exports = function (options) {
+  const { scripts = [], styles = [], ...optionsRest } = options
   const theme = mandelbrot({
-    scripts: ['/theme-wave/js/wave.js'],
-    styles: ['/theme-wave/css/wave.css'],
-    ...options,
+    scripts: ['/theme-wave/js/wave.js', ...scripts],
+    styles: ['/theme-wave/css/wave.css', ...styles],
+    ...optionsRest,
   })
   theme.addLoadPath(__dirname + '/views')
   theme.addStatic(__dirname + '/dist', '/theme-wave')
